d6a7e14b1de17914e52b2d47049e01f06175fa39
Yanjie Mac <yanjiezheng@berkeley.edu>
2023-06-17 18:04:10 -0700

    trusted origins

--- Path/settings.py
+++ Path/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-f67ih!g41dha0k8wmh38d&pu==m=lr_!p)2#v_msl4*w$qg3n!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -99,7 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
 CSRF_TRUSTED_ORIGINS = ["https://pathai.azurewebsites.net"]
+CSRF_COOKIE_SECURE = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
