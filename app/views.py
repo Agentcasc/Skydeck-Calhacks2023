497d4acd37d784b33ce87d9f613bf918a3367214
Yanjie Mac <yanjiezheng@berkeley.edu>
2023-06-18 13:45:43 -0700

    a

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 # Create your views here.
 
-
+@login_required
 def main(request):
     return render(request, "main.html")
 
